e69ffb820129a38658bf36f5a3305ac4a9591a6c
Aleksander Misztal <apm81@cam.ac.uk>
2022-02-19 10:41:42 +0000

    Use smaller images

--- components/CoinInfo.js
+++ components/CoinInfo.js
@@ -9,11 +9,7 @@ export default function CoinInfo({ coin, ...props }) {
       className="flex items-center rounded-md bg-slate-100 p-2 shadow-md hover:scale-[1.02] hover:cursor-pointer"
       {...props}
     >
-      <img
-        className="w-10 h-10 rounded-full mr-4"
-        src={coin.large}
-        alt={coin.name}
-      />
+      <img className="w-10 h-10 mr-4" src={coin.img} alt={coin.name} />
       <div className="text-sm">
         <p className="text-gray-900 leading-none">{coin.name}</p>
         <p className="text-gray-600">{shortAddr}</p>
